33f1f9f13b637aab857fe8e806f9ee01ef728dd6
MichaelYang21 <zzyang21@gmail.com>
2018-09-07 18:05:20 -0400

    Update tl_detector.py
    Defined self.waypoints in the function waypoints_cb
    Returned light.state in the function get_light_state without using classifier
    Added "return closest_idx" in the function get_closest_waypoint

--- ros/src/tl_detector/tl_detector.py
+++ ros/src/tl_detector/tl_detector.py
@@ -59,6 +59,7 @@ class TLDetector(object):
         self.pose = msg
 
     def waypoints_cb(self, waypoints):
+        self.waypoints = waypoints
         if not self.waypoints_2d:
             self.waypoints_2d = [[waypoint.pose.pose.position.x, waypoint.pose.pose.position.y] for waypoint in waypoints.waypoints]
             self.waypoints_tree = KDTree(self.waypoints_2d)
@@ -107,7 +108,7 @@ class TLDetector(object):
 
         """
         closest_idx = self.waypoints_tree.query([x,y],1)[1]
-
+        return closest_idx
 
     def get_light_state(self, light):
         """Determines the current color of the traffic light
@@ -119,14 +120,15 @@ class TLDetector(object):
             int: ID of traffic light color (specified in styx_msgs/TrafficLight)
 
         """
-        if(not self.has_image):
-            self.prev_light_loc = None
-            return False
+        #if(not self.has_image):
+        #    self.prev_light_loc = None
+        #    return False
 
-        cv_image = self.bridge.imgmsg_to_cv2(self.camera_image, "bgr8")
+        #cv_image = self.bridge.imgmsg_to_cv2(self.camera_image, "bgr8")
 
-        #Get classification
-        return self.light_classifier.get_classification(cv_image)
+        ##Get classification
+        #return self.light_classifier.get_classification(cv_image)
+        return light.state
 
     def process_traffic_lights(self):
         """Finds closest visible traffic light, if one exists, and determines its
